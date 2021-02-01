c5d06e5cda269989a3ffdf101b6a007501b497bd
YasmeenTak <yasmeen.tak.33@gmail.com>
2021-02-01 09:42:31 +0200

    news ticker done

--- js/index.js
+++ js/index.js
@@ -22,66 +22,47 @@ $(function () {
   });
 });
 
-$(function () {
-  $(document).ready(function () {
-    $('#newsTicker2').breakingNews({
-      direction: 'rtl',
+
+var $ticker = $('[data-ticker="list"]'),
+  tickerItem = '[data-ticker="item"]';
+(itemCount = $(tickerItem).length), (viewportWidth = 0);
+
+function setupViewport() {
+  $ticker.find(tickerItem).clone().prependTo('[data-ticker="list"]');
+
+  for (i = 0; i < itemCount; i++) {
+    var itemWidth = $(tickerItem).eq(i).outerWidth();
+    viewportWidth = viewportWidth + itemWidth;
+  }
+
+  $ticker.css('width', viewportWidth);
+}
+
+function animateTicker() {
+  $ticker.animate(
+    {
+      marginRight: -viewportWidth,
+    },
+    30000,
+    'linear',
+    function () {
+      $ticker.css('margin-right', '0');
+      animateTicker();
+    }
+  );
+}
+
+function initializeTicker() {
+  setupViewport();
+  animateTicker();
+
+  $ticker
+    .on('mouseenter', function () {
+      $(this).stop(true);
+    })
+    .on('mouseout', function () {
+      animateTicker();
     });
-  });
-});
+}
 
-// var www = document.querySelector('.wrp'),
-//   ttt = document.querySelector('.ticker'),
-//   mov = document.querySelector('.moving');
-
-/*
-www.addEventListener('mouseover',pause,false);
-www.addEventListener('mouseleave',play,false);
-function pause(){ttt.style.animationPlayState="paused";}
-function play(){ttt.style.animationPlayState="running";}
-*/
-
-// ttt.addEventListener('animationiteration', cnsl, false);
-// //ttt.addEventListener("animationstart", shortLoop, false);
-// ttt.addEventListener('animationstart', strt, false);
-
-// function cnsl() {
-//   console.log('fds');
-// }
-// function strt() {
-//   var movCoor = mov.getBoundingClientRect(),
-//     wrapCoor = www.getBoundingClientRect(),
-//     duration = ((movCoor.width / wrapCoor.width) * 20).toFixed() + 's';
-//   durationAttribute = 'animation-duration:' + duration;
-//   console.log(duration);
-//   ttt.setAttribute('style', durationAttribute);
-// }
-
-// function shortLoop() {
-//   ttt.classList.remove('anim');
-//   console.log('now');
-//   ttt.offsetWidth = ttt.offsetWidth;
-//   setTimeout(function () {
-//     ttt.classList.add('anim');
-//     console.log('now');
-//   }, 10);
-// }
-
-// setInterval(function () {
-//   var movCoor = mov.getBoundingClientRect(),
-//     tickCoor = ttt.getBoundingClientRect(),
-//     wrapCoor = www.getBoundingClientRect();
-
-//   console.log(movCoor.left, wrapCoor.left + wrapCoor.width);
-//   // console.log(wrapCoor.width);
-//   // console.log(wrapCoor.left);
-
-//   var duration = (movCoor.width / wrapCoor.width) * 10 + 's';
-//   console.log(duration);
-
-//   if (wrapCoor.left + wrapCoor.width < movCoor.left) {
-//     if (movCoor.left > 0) {
-//       shortLoop();
-//     }
-//   }
-// }, 1000 / 1);
+initializeTicker();
