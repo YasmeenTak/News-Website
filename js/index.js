44dd3683491b20f44e50606d9849eeab581a767c
YasmeenTak <yasmeen.tak.33@gmail.com>
2021-02-04 16:11:59 +0200

    add animation

--- js/index.js
+++ js/index.js
@@ -63,3 +63,35 @@ function initializeTicker() {
 }
 
 initializeTicker();
+
+$(function () {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1000: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
